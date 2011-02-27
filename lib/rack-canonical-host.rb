d06b2419d227c63016bb0e500ef7ade0aa126308
Don Buchanan <mail@donaldbuchanan.com>
2011-02-28 08:23:09 +1100

    only redirect www

--- lib/rack-canonical-host.rb
+++ lib/rack-canonical-host.rb
@@ -17,7 +17,7 @@ module Rack # :nodoc:
     def url(env)
       if (host = host(env)) && env['SERVER_NAME'] != host
         url = Rack::Request.new(env).url
-        url.sub(%r{\A(https?://)(.*?)(:\d+)?(/|$)}, "\\1#{host}\\3/")
+        url.sub(%r{\A(https?://)www(.*?)(:\d+)?(/|$)}, "\\1#{host}\\3/")
       end
     end
     private :url
